e4883182c17c9fd30993317131a21693caa89d31
Samuel581 <00023020@uca.edu.sv>
2022-04-30 17:46:19 -0600

    Glide + (Dis)able Text Imput

--- app/src/main/java/com/example/mdc/ScrollingActivity.kt
+++ app/src/main/java/com/example/mdc/ScrollingActivity.kt
@@ -7,6 +7,10 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.mdc.databinding.ActivityScrollingBinding
 import com.google.android.material.bottomappbar.BottomAppBar
 
@@ -39,6 +43,31 @@ class ScrollingActivity : AppCompatActivity() {
                 .show()
         })
 
+
+        //Buttons
+
+        binding.content.btnSkip!!.setOnClickListener{binding.content.cvAd!!.visibility = View.GONE}
+        binding.content.btnBuy!!.setOnClickListener{
+                Snackbar.make(it, "Item bought", Snackbar.LENGTH_LONG)
+                    .setAnchorView(binding.fab)
+                    .setAction("See", {
+                        Toast.makeText(this, "Historial", Toast.LENGTH_SHORT).show()
+                    })
+                    .show()
+        }
+
+        //Get image from url
+
+        Glide.with(this)
+            .load("https://i.pinimg.com/236x/a9/d0/b0/a9d0b05ed598cf00102163a1dd969ebf.jpg")
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(binding.content.imgCover!!)
+
+        //Enable / disble password
+        binding.content.cnEnablePass!!.setOnClickListener{
+            binding.content.passwordTextInput!!.isEnabled = !binding.content.passwordTextInput!!.isEnabled
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
